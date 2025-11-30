e5b5ca4b455d06e9b4a8eeb3682625f5b525f696
lfs <lidgnulinux@gmail.com>
2025-11-30 07:00:17 +0700

    add lfs.port.mk & curl ports.

---
+++ lfs.port.mk
@@ -0,0 +1,86 @@
+PREFIX=/usr
+LIBDIR=/usr/lib
+SYSCONFDIR=/etc
+LIBEXECDIR=/usr/libexec
+BINDIR=/usr/bin
+
+download:
+ifeq ($(SRCCD),archive)
+	@echo "Mode: Archive"
+	@if [ ! -f ${ARCHIVE} ]; then \
+		echo "File not found. Downloading ${ARCHIVE_FILE}..."; \
+		wget -c ${LINK}; \
+	else \
+		echo "Archive ${ARCHIVE} already exists. Skipping."; \
+	fi
+else ifeq ($(SRCCD),git)
+	@echo "Mode: Git"
+	@if [ ! -d ${PKGNAME} ]; then \
+		echo "Directory not found. Cloning git repo..."; \
+		git clone ${HOMEPAGE}; \
+	else \
+		echo "Git directory ${PKGNAME} already exists. Skipping."; \
+	fi
+else
+	$(error Unknown SRCCD: ${SRCCD}. Valid options are 'archive' or 'git')
+endif
+
+
+extract:
+	tar -xvf ${ARCHIVE} 
+
+prepare:
+	${PREPARE}
+
+build:
+ifeq ($(BUILD),meson)
+	@echo "Mode: Build Meson"
+	meson setup ${BUILD_OPTION} build ${BUILDDIR}
+	meson compile -C build
+else ifeq ($(BUILD),make)
+	@echo "Mode: Build Make"
+	${AUTO_CONF}
+	make -j4 -C ${BUILDDIR} PREFIX=${PREFIX}
+else ifeq ($(BUILD),cmake)
+	@echo "Mode: Build Cmake"
+	cmake -B build -G Ninja -S ${BUILDDIR} ${BUILD_OPTION}
+	cmake --build build
+else
+	$(error Unknown BUILD: ${BUILD}. Valid options are 'meson', 'make', or 'cmake')
+endif
+
+
+package:
+ifeq ($(BUILD),meson)
+	@echo "Mode: Package Meson"
+	meson install --no-rebuild -C build --destdir package
+	install  -Dm644 $(PWD)/Makefile $(PWD)/build/package/var/lib/mk/${PKGNAME}.mk
+	$(POST_BUILD)
+	tar -C build/package -cvf \
+		${PKGNAME}_${VERSION}_${REVISION}_@${CATEGORIES}.tar.gz .
+else ifeq ($(BUILD),make)
+	@echo "Mode: Package Make"
+	make -j4 -C ${BUILDDIR} PREFIX=${PREFIX} DESTDIR="$(PWD)/package" install
+	install  -Dm644 $(PWD)/Makefile $(PWD)/package/var/lib/mk/${PKGNAME}.mk
+	$(POST_BUILD)
+	tar -C package -cvf \
+		${PKGNAME}_${VERSION}_${REVISION}_@${CATEGORIES}.tar.gz .
+else ifeq ($(BUILD),cmake)
+	@echo "Mode: Package Cmake"
+	DESTDIR="package" cmake --install build
+	install  -Dm644 $(PWD)/Makefile $(PWD)/package/var/lib/mk/${PKGNAME}.mk
+	$(POST_BUILD)
+	tar -C package -cvf \
+		${PKGNAME}_${VERSION}_${REVISION}_@${CATEGORIES}.tar.gz .
+else
+	$(error Unknown BUILD: ${BUILD}. Valid options are 'meson', 'make', or 'cmake')
+endif
+
+
+install-package:
+	echo "Installing package archive ${PKG}"
+	echo "make directory for $(shell basename -s .tar.gz ${PKG})"
+	mkdir /usr/pkg/$(shell basename -s .tar.gz ${PKG})
+	echo "extracting package archive ${PKG}"
+	tar -xvf ${PKG} -C /usr/pkg/$(shell basename -s .tar.gz ${PKG})
+	graft -i -P -t / /usr/pkg/$(shell basename -s .tar.gz ${PKG})
